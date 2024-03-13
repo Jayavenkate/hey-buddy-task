4df6b53bec029f01a65fb33cd16b4345e4f046d9
Jayavenkate <jayajaikithuja@gmail.com>
2024-03-14 01:23:44 +0300

    ui

--- src/Twitter Page.jsx
+++ src/Twitter Page.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom"
 
 export const Twitter = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
     return (
         <div>
             <button onClick={() => navigate("/")}>Back</button>
-            <p>Welcome </p>
+            <h1 style={{ textAlign: "center" }}>Welcome </h1>
         </div>
     )
 }
